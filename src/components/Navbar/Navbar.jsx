6c553bb7728effc5bb779a55cd65a2b56266c687
Rambo-react <sandstorm0819@gmail.com>
2021-03-05 16:03:21 +0300

    Add routes and components:dialogs,profile,news...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,24 @@
 import s from './Navbar.module.css';
 
-//console.log(s);
-// let s = {
-//   'nav' : 'Navbar_nav__3jhg3',
-//   'item' : 'Navbar_item__3dfgd',
-//   'active' : 'Navbar_sdfsdf'
-// }
 
-// let c1 = 'item';
-// let c2 = 'active';
-// //'item active'
-// let classes = c1 + ' ' + c2;
-// let classesNew = `${s.item} ${s.active}`
 
 const Navbar = () => {
     return (
       <nav className={s.nav}>
       <div className={s.item}>
-       <a>Profile</a>
+       <a href="/profile">Profile</a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-       <a>Messages</a>
+       <a  href="/dialogs">Messages</a>
       </div>
       <div className={s.item}>
-       <a>News</a>
+       <a href='/news'>News</a>
       </div>
       <div className={s.item}>
-       <a>Music</a>
+       <a href="/music">Music</a>
       </div>
       <div className={s.item}>
-       <a>Settings</a>
+       <a href='/settings'>Settings</a>
       </div>
     </nav>
     );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,17 @@
 import s from './Profile.module.css';
-import MyPosts from './MyPosts/MyPosts.jsx'
+import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-    return (
-        <div className={s.content}>
-        <div>
+  return (
+    <div>
+      <div>
         <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg"></img>
-        </div>
-        <div>
-          ava+description
-        </div>
-        <MyPosts />
       </div>
-    );
+      <div>
+        ava+description
+        </div>
+      <MyPosts />
+    </div>
+  );
 }
 export default Profile;
