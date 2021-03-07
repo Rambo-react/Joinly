2a567c53eda598b9891bacbf7a558ad9bdb4b83e
Rambo-react <sandstorm0819@gmail.com>
2021-03-07 19:08:59 +0300

    Modified Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,34 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Petr
+                </div>
+                <div className={s.dialog}>
+                    Igor
+                </div>
+                <div className={s.dialog}>
+                    Vladimir
+                </div>
+                <div className={s.dialog}>
+                    Aleksey
+                </div>    
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Are you fine?</div>
+                <div className={s.message}>What are your name?</div>
+
+            </div>
+            
         </div>
     )
 }
