20240882d06d44e5f05a63bb6e04b5f9acd288bc
Rambo-react <sandstorm0819@gmail.com>
2021-04-08 20:16:13 +0300

    refactoring usersContainer, add class component to usersContainer, divide to pure function Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,48 +1,28 @@
 import React from 'react';
 import s from './Users.module.css'
-import * as axios from 'axios'; //ипортируем всё что экспортируется в библиотеке axios с названием "axios" 
 import userPhoto from '../../assets/images/images.png';
 
-class Users extends React.Component {
+let Users = (props) => {
+    //расчет количества страниц и создание массива для этих номеров(math.ceil округление до целого числа в большую сторону)
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-        .then(response => { //response - ответ  
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-        });
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onPageChanged =(pageNumber) => {
-        //диспатчим в стэйт номер страницы, потом сразу же выполняем запрос на сервер 
-        //, по этому мы написали в get запросе page=pageNumber, т.к. в пропсах пока что старые значения, а pageSize нас менять не надо
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => { //response - ответ , 
-            this.props.setUsers(response.data.items)
-        });
-    }
-
-    render() {
-
-        //расчет количества страниц и создание массива для этих номеров(math.ceil округление до целого числа в большую сторону)
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return (<div>
+    return (
+        <div>
             <div>
-                { pages.map(p => {
-                        return (<span className={ this.props.currentPage ===  p && s.selectedPage }
-                            onClick={ (e) => { this.onPageChanged(p)  } } >{p}</span>)
-                    })}
+                {pages.map(p => {
+                    return (<span className={props.currentPage === p && s.selectedPage}
+                        onClick={(e) => { props.onPageChanged(p) }} >{p}</span>)
+                })}
 
             </div>
             {
-                this.props.users.map(u => <div key={u.id}>
+                props.users.map(u => <div key={u.id}>
                     <div className={s.Users_grid}>
                         <div className={s.user_img_btn}>
                             <div>
@@ -50,8 +30,8 @@ class Users extends React.Component {
                             </div>
                             <div>
                                 {u.followed ?
-                                    <button onClick={() => { this.props.unfollow(u.id) }} >Unfollow</button> :
-                                    <button onClick={() => { this.props.follow(u.id) }} >Follow</button>
+                                    <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button> :
+                                    <button onClick={() => { props.follow(u.id) }} >Follow</button>
                                 }
 
 
@@ -86,8 +66,8 @@ class Users extends React.Component {
 
 
 
-        </div>)
-    }
+        </div>
+    )
 }
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,43 @@
+import React from 'react';
 import { connect } from "react-redux";
 import { followAC, setCurrentPageAC, setUsersAC, unfollowAC, setTotalUsersCountAC } from "../../redux/users-reducer";
 import Users from "./Users";
+import * as axios from 'axios'; //ипортируем всё что экспортируется в библиотеке axios с названием "axios" 
 
+class UsersContainer extends React.Component {
 
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        .then(response => { //response - ответ  
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged =(pageNumber) => {
+        //диспатчим в стэйт номер страницы, потом сразу же выполняем запрос на сервер 
+        //, по этому мы написали в get запросе page=pageNumber, т.к. в пропсах пока что старые значения, а pageSize нас менять не надо
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => { //response - ответ , 
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+
+        return (
+            <Users totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+
+            />
+        )
+    }
+}
 
 let mapStateToProps = (state) => {
     return ({
@@ -24,6 +59,4 @@ let mapDispatchToProps = (dispatch) => {
 }
 
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
