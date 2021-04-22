ea244002db252d02947817e50e0baeb97e19b239
Rambo-react <sandstorm0819@gmail.com>
2021-04-23 00:09:14 +0300

    added: 1) hooks in ProfileStatusWithHooks 2) shouldComponentUpdate, PureComponent, React.memo in MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Field, Form, reduxForm } from 'redux-form';
 import { addPost } from '../../../redux/profile-reducer';
@@ -16,7 +16,7 @@ const PostForm = (props) => {
   return (
     <Form onSubmit={props.handleSubmit}>
       <div>
-        <Field name={"postText"} component={Textarea} placegolder={"Enter your post text"} validate={[required, maxLength10]}/>
+        <Field name={"postText"} component={Textarea} placegolder={"Enter your post text"} validate={[required, maxLength10]} />
       </div>
       <div>
         <button>Add Post</button>
@@ -28,19 +28,62 @@ const PostForm = (props) => {
 
 const PostReduxForm = reduxForm({ form: "addPost" })(PostForm);
 
+//------------------наследуем класс PureComponent что бы , не писать метод жизненного цикла shouldComponentUpdate
+//--------------------------------МОЖНО ТОЖЕ САМОЕ СДЕЛАТЬ И ФУНКЦИОНАЛЬНЫМ КОМПОНЕТННОМ
 
-const MyPosts = (props) => {
+// class MyPosts extends PureComponent  {
 
-  const addNewPost = (formData) => {
-    props.addPost(formData.postText);
+//   //PureComponent заменяет shouldComponentUpdate, автоматически делает shouldComponentUpdate
+//   //-------------- shouldComponentUpdate проверяет нужно ли перерисовывать компонент, в пропасах сидит следующие пропсы и стэйт
+//   //---------------например по таким условиям которые указаны в функции ниже
+//   // shouldComponentUpdate(nextProps, nextState) {
+//   //   return nextProps != this.props || nextState !=this.state;
+//   // }
 
-  }
+//   render() {
+//     const addNewPost = (formData) => {
+//     this.props.addPost(formData.postText);
 
+//   }
+
+
+//   let postsElements = this.props.postsData.map((p) => {
+//     return (<Post message={p.message} key={p.id} likesCount={p.likesCount} />)
+//   });
+
+//   return (
+
+//     <div className="postsBlock">
+
+//       <h3>MyPosts</h3>
+//       <PostReduxForm onSubmit={addNewPost} />
+
+//       <div className={s.posts}>
+//         {postsElements}
+
+//       </div>
+//     </div>
+
+//   )
+//   }
+
+// }
+
+
+//-------------------------------ФУНКЦИОНАЛЬНЫЙ КОМПОНЕНТ
+//-------------------------- оборачиваем компонент MyPosts, хоком React.memo 
+const MyPosts = React.memo( (props) => {
 
   let postsElements = props.postsData.map((p) => {
     return (<Post message={p.message} key={p.id} likesCount={p.likesCount} />)
   });
 
+  let newPostElement = React.createRef();
+
+  const addNewPost = (formData) => {
+    props.addPost(formData.postText);
+
+  }
   return (
 
     <div className="postsBlock">
@@ -54,9 +97,10 @@ const MyPosts = (props) => {
       </div>
     </div>
 
-  );
-}
+  )
 
+
+})
 
 
 export default MyPosts;
--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ProfileStatusWithHooks =(props) => {
 
 
         //useState возвращает массив, первый элемент - это значение, второй функция которая будет изменять это значение
-        let stateWithSetState = useState(false);
+     //   let stateWithSetState = useState(false);
         //элемент со значением
-        let editMode = stateWithSetState[0];
+     //   let editMode = stateWithSetState[0];
         //фыункция которая будет устанавливать значение
-        let setEditMode = stateWithSetState[1];
+      //  let setEditMode = stateWithSetState[1];
 
+        let [editMode, setEditMode] = useState(false);
+        let [status, setStatus] = useState(props.status);
+    // useEffect выполняется после полной отрисовки, и потом если изменился статус в пропсах
+        useEffect( () => {
+            setStatus(props.status);
+
+        }, [props.status])
+
+        const activateEditMode = () => {
+            setEditMode(true);
+        }
+
+        const  deactivateEditMode = () => {
+            setEditMode(false);
+            props.updateStatus(status);
+        }
+
+        const onStatusChange = (e) => {
+                setStatus(e.currentTarget.value);
+        }
 
         return (
             < div >
                 {!editMode &&
                     <div>
-                        <span >{props.status || "------HAVEN'T---STATUS-------"}</span>
+                        <span onDoubleClick={activateEditMode}  >{props.status || "------HAVEN'T---STATUS-------"}</span>
                     </div>
                 }
 
                 {editMode &&
                     <div>
-                        <input />
+                        <input onChange={onStatusChange} autoFocus={true} onBlur={deactivateEditMode} value={status} />
                     </div>
                 }
 
